79c1a5eb6c89843cc8c7256a1f228882c6167d03
LuQmu5 <42968850+Lqms@users.noreply.github.com>
2023-08-31 15:38:21 +0400

    1

--- Assets/Scripts/UI/Parallax.cs
+++ Assets/Scripts/UI/Parallax.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(RawImage))]
 public class Parallax : MonoBehaviour
 {
+    [SerializeField] private float _speed = 0.05f;
 
+    private RawImage _backgroundImage;
+    private float _offsetX;
+
+    private void Start()
+    {
+        _backgroundImage = GetComponent<RawImage>();    
+    }
+
+    private void Update()
+    {
+        _offsetX += _speed * Time.deltaTime;
+
+        if (_offsetX >= 1)
+            _offsetX = 0;
+
+        _backgroundImage.uvRect = new Rect(_offsetX, _backgroundImage.uvRect.y, _backgroundImage.uvRect.width, _backgroundImage.uvRect.height);
+    }
 }
